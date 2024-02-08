503023e04b7468e11a63221e3246ac8ead66ff6e
SergeyRusin83 <sergei-rusin@yandex.ru>
2024-02-08 12:41:00 +0300

    Создан заголовочный файл

--- Hello World1/Hello World1.cpp
+++ Hello World1/Hello World1.cpp
@@ -1,9 +1,16 @@
 ﻿#include <iostream>
+#include "Header.h"
+
 
 using namespace std;
 
 int main()
 {
+    int a;
+    cin >> a;
     cout << "Hello World!\n";
+    cout << endl;
+
+    foo(a);
 }
 
